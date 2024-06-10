3062acdb689db2a744b516054ba301c91c5f2237
Kirill <123@mail.ru>
2024-06-10 15:36:53 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+a = 5
+console.log(a)
+let persone = {
+	name: "Kirill",
+	age: 25,
+	isMaried: false
+}
+
+console.log(persone.name)
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+
+console.log(arr[3])
+
+alert('Hello World')
+
+confirm()
+
+let one = '1'
